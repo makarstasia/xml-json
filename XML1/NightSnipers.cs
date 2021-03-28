1c792568fdd6c80f9c84224c512bc1be4cd5a466
Anastasia Makarshina <makarshinaa1@gmail.com>
2021-03-28 16:38:15 +0300

    deserialize from xml

---
+++ XML1/NightSnipers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace XML1
+{
+    public partial class NightSnipers : Form
+    {
+        public NightSnipers()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlDocument i = new XmlDocument();
+            DataSet ds = new DataSet();
+            XmlReader xmlFile;
+            xmlFile = XmlReader.Create(@"nightsnipers.xml", new XmlReaderSettings());
+            ds.ReadXml(xmlFile);
+            dataGridView1.DataSource = ds.Tables[0];
+            xmlFile.Close();
+        }
+
+    }
+}
